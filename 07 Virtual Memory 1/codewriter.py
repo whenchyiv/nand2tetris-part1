027302df3d5502bf2c53be4591989e1e46b53a53
Will <will.henchy@gmail.com>
2025-10-21 16:26:27 -0400

    comments

--- 07 Virtual Memory 1/codewriter.py
+++ 07 Virtual Memory 1/codewriter.py
@@ -51,13 +51,16 @@ class CodeWriter(object):
         asm: str = ""
         pointer_name: str = ram.NAMED_REGISTER_NAMES[str(command.arg1)]
         value: str | None = command.arg2
+        # Push assmebly generation
         if command.command_type == self._command_types.push:
             asm += f"@{value}\nD=A\n"  # Store the value in the D register
             asm += f"@{pointer_name}\nA=M\nM=D\n"  # Push the D register value onto the relevant stack
             asm += f"@{pointer_name}\nM=M+1\n"  # Increment the stack pointer
+        # Pop assmebly generation
         elif command.command_type == self._command_types.pop:
             asm += f"@{pointer_name}\nM=M-1\n"  # Decrement the stack pointer value
             asm += f"@{pointer_name}\nD=M\n"  # Read the value from the top of the stack and store in the D register
+        # Wtf? We should never get here.
         else:
             raise ValueError(
                 f"Unknown command type for pushpop command: {command.command_type}"
