f9a8c613cf60c6193784d824f8c0e78f3a1aee93
Will <will.henchy@gmail.com>
2025-10-25 15:32:58 -0400

    filename tweak

--- 07 Virtual Memory 1/codewriter.py
+++ 07 Virtual Memory 1/codewriter.py
@@ -310,7 +310,7 @@ class CodeWriter(object):
         line_count: int = 0
         with open(self.output_filename, "w") as file:
             file.write(
-                f"// {self.vm_filename} translated to Hack assembly by Interpres\n"
+                f"// {self.vm_filename.split('/')[-1]} translated to Hack assembly by Interpres\n"
             )
             for line, token_list in self._parser:
                 if debug:  # Include VM tokens as a comment for debugging if requested via the debug var.
