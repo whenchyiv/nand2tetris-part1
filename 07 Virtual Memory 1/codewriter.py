2cb4e4d1e150a7251fdc7c1df725c55ffa765881
Will <will.henchy@gmail.com>
2025-10-25 15:29:02 -0400

    remove todo

--- 07 Virtual Memory 1/codewriter.py
+++ 07 Virtual Memory 1/codewriter.py
@@ -148,8 +148,6 @@ class CodeWriter(object):
                 f"Missing command for arithmetic command at line {line_number}."
             )
 
-        # TODO: Add comparison and logical commands
-
         asm: str = ""
         if vm_command == "add":
             asm = """\
